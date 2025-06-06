a145c15a8ae63f37c4c6b5a74a7e83cce039d1a5
Abhinav Pareek <166374677+abhinavpareek655@users.noreply.github.com>
2025-06-06 11:02:28 +0530

    Update page.tsx

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,12 +1,1 @@
-"use client";
-import { useSearchParams } from "next/navigation";
 
-export default function Page() {
-  const searchParams = useSearchParams();
-  const email = searchParams.get("email");
-  return (
-    <div>
-      <p>Email: {email}</p>
-    </div>
-  );
-}
